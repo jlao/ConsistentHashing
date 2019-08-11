f5be97717fdce85b9f1b86b8ede3bf6fc9ba5119
James Lao <git@jameslao.com>
2019-08-11 00:45:47 -0700

    Add comments to IConsistentHashRing and cleanup.

--- ConsistentHashing/IConsistentHashRing.cs
+++ ConsistentHashing/IConsistentHashRing.cs
@@ -1,19 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConsistentHashing
+﻿namespace ConsistentHashing
 {
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents a consistent hash ring.
+    /// </summary>
+    /// <typeparam name="TNode"></typeparam>
     public interface IConsistentHashRing<TNode> : IEnumerable<(TNode, uint)>
+        where TNode : IComparable<TNode>
     {
+        /// <summary>
+        /// Gets all partitions where a partition is a hash range and the owner node.
+        /// </summary>
         IEnumerable<Partition<TNode>> Partitions { get; }
 
+        /// <summary>
+        /// Gets whether the consistent hash ring is empty or not.
+        /// </summary>
         bool IsEmpty { get; }
 
+        /// <summary>
+        /// Adds the specified node with virtual nodes.
+        /// </summary>
+        /// <param name="node">The node to add.</param>
+        /// <param name="virtualNodes">One or more points on the hash ring to add the node to.</param>
         void AddNode(TNode node, IEnumerable<uint> virtualNodes);
 
+        /// <summary>
+        /// Removes all virtual nodes for the specified node from the hash ring.
+        /// </summary>
+        /// <param name="node">The node to remove.</param>
         void RemoveNode(TNode node);
 
+        /// <summary>
+        /// Gets the node that owns the hash.
+        /// </summary>
+        /// <param name="hash">The hash.</param>
+        /// <returns>The node that owns the hash.</returns>
         TNode GetNode(uint hash);
     }
 }
