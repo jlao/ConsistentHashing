15aa1008bf94626ba113d4e95fc0759a4d333bb4
James Lao <git@jameslao.com>
2019-08-11 17:17:33 -0700

    Add Contains method to HashRange.

--- src/ConsistentHashing/HashRange.cs
+++ src/ConsistentHashing/HashRange.cs
@@ -25,5 +25,15 @@
         /// Gets the inclusive end of the hash range.
         /// </summary>
         public uint EndInclusive { get; }
+
+        /// <summary>
+        /// Checks whether the specified hash is contained in the range.
+        /// </summary>
+        /// <param name="hash">The hash to check.</param>
+        /// <returns>True if the hash is in the range and false otherwise.</returns>
+        public bool Contains(uint hash)
+        {
+            return hash > this.StartExclusive && hash <= this.EndInclusive;
+        }
     }
 }
---
+++ src/UnitTests/HashRangeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace UnitTests
+{
+    using ConsistentHashing;
+    using FluentAssertions;
+    using Xunit;
+
+    public class HashRangeTests
+    {
+        [Fact]
+        public void ContainsTest()
+        {
+            var range = new HashRange(10, 15);
+
+            for (uint i = 11; i <= 15; i++)
+            {
+                range.Contains(i).Should().BeTrue();
+            }
+
+            range.Contains(10).Should().BeFalse();
+            range.Contains(16).Should().BeFalse();
+        }
+    }
+}
